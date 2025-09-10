32bf1e335f471ed9fd8940877c8ef2749dee87e7
梁培源 <liangpeiyuan@yingzi.com>
2025-09-10 21:32:25 +0800

    fit

--- mian.dart
+++ mian.dart
@@ -164,18 +164,19 @@ void main() {
 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
  */
 List<int> twoSum(List<int> nums, int target) {
-    Map<int, int> numToIndex = {};
-    for (int i = 0; i < nums.length; i++) {
-      int expectedNum = target - nums[i];
-      if(numToIndex.containsKey(expectedNum)){
-        return [numToIndex[expectedNum]!, i];
-      }else{
-        numToIndex[nums[i]] = i;
-      }
+  Map<int, int> numToIndex = {};
+  for (int i = 0; i < nums.length; i++) {
+    int expectedNum = target - nums[i];
+    if (numToIndex.containsKey(expectedNum)) {
+      return [numToIndex[expectedNum]!, i];
+    } else {
+      numToIndex[nums[i]] = i;
     }
-    return [];
   }
-  /**
+  return [];
+}
+
+/**
    2. 两数相加
 给你两个 非空 的链表，表示两个非负的整数。它们每位数字都是按照 逆序 的方式存储的，并且每个节点只能存储 一位 数字。
 请你将两个数相加，并以相同形式返回一个表示和的链表。
@@ -185,22 +186,23 @@ List<int> twoSum(List<int> nums, int target) {
 解释：342 + 465 = 807.
    */
 ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
-     ListNode dummy = ListNode(0);
-    ListNode current = dummy;
-    int upTen = 0;
-    while (l1 != null || l2 != null || upTen > 0) {
-      int val1 = l1?.val ?? 0;
-      int val2 = l2?.val ?? 0;
+  ListNode dummy = ListNode(0);
+  ListNode current = dummy;
+  int upTen = 0;
+  while (l1 != null || l2 != null || upTen > 0) {
+    int val1 = l1?.val ?? 0;
+    int val2 = l2?.val ?? 0;
 
-      int sum = (val1 + val2 + upTen) % 10;
-      upTen = (val1 + val2 + upTen) ~/ 10;
-      current.next = ListNode(sum);
-      current = current.next!;
-      l1 = l1?.next;
-      l2 = l2?.next;
-    }
-    return dummy.next;
+    int sum = (val1 + val2 + upTen) % 10;
+    upTen = (val1 + val2 + upTen) ~/ 10;
+    current.next = ListNode(sum);
+    current = current.next!;
+    l1 = l1?.next;
+    l2 = l2?.next;
   }
+  return dummy.next;
+}
+
 /**
  3. 无重复字符的最长子串
 给定一个字符串 s ，请你找出其中不含有重复字符的 最长 子串 的长度。
@@ -209,23 +211,20 @@ ListNode? addTwoNumbers(ListNode? l1, ListNode? l2) {
 输出: 3 
 解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
  */
-  int lengthOfLongestSubstring(String s) {
-    int slow = 0;
-    int maxLength = 0;
-    Set<String> sSet = {};
-    for (int fast = 0; fast < s.length; fast++) {
-      
-
-      while (sSet.contains(s[fast])) {
-        sSet.remove(s[slow]);
-        slow++;
-      }
-      sSet.add(s[fast]);
-      maxLength = maxLength > (fast - slow + 1) ? maxLength : (fast - slow + 1);
-
+int lengthOfLongestSubstring(String s) {
+  int slow = 0;
+  int maxLength = 0;
+  Set<String> sSet = {};
+  for (int fast = 0; fast < s.length; fast++) {
+    while (sSet.contains(s[fast])) {
+      sSet.remove(s[slow]);
+      slow++;
     }
-    return maxLength;
+    sSet.add(s[fast]);
+    maxLength = maxLength > (fast - slow + 1) ? maxLength : (fast - slow + 1);
   }
+  return maxLength;
+}
 
 /**
  4. 寻找两个正序数组的中位数
@@ -278,6 +277,7 @@ double findMedianSortedArrays(List<int> nums1, List<int> nums2) {
     return midValue;
   }
 }
+
 /**
  5. 最长回文子串
 给你一个字符串 s，找到 s 中最长的 回文 子串。
@@ -363,6 +363,7 @@ String convert(String s, int numRows) {
 
   return result.join(''); // return the final string
 }
+
 /**
  7. 整数反转
 已解答
@@ -4869,8 +4870,6 @@ push(value)：如果push的元素value大于入口元素的数值，那么就将
   }
   return result;
 }
-
-
 
 // 数组 / 字符串
 // 合并两个有序数组
